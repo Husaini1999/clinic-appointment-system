3b9400550beb667d01d21652e2842d78c280df7a
Husaini1999 <husainimuhd99@yahoo.com>
2024-11-13 01:13:42 +0800

    updated frontend and backend code for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ app.use(helmet());
 app.use(express.json({ limit: '10mb' }));
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
+		origin:
+			process.env.NODE_ENV === 'production'
+				? process.env.FRONTEND_URL
+				: 'http://localhost:3000',
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
